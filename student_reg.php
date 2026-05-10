514140a26ccd8f6ed4bdbce2510ea7ba602767c5
isinduanjana <isinduanjana78@gmail.com>
2026-05-10 16:09:44 +0530

    Student registration

---
+++ student_reg.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<html>
+    <title>Student registration</title>
+    <head>
+    <link rel="stylesheet" href="student_reg.css">
+    </head>
+    <body>
+        <h1> Student registration </h1>
+    <form method="post" action="">
+        <p>TG NO <input type="text" name="tg"><br>
+        E-mail <input type="text" name="email"><br>
+        Password <input type="text" name="pass"><br>
+        Confirm password <input type="text" name="confp"><br>
+        Request Room <input type="text" name="rq"><br></p>
+    <button name="submi" id="submit">SUBMIT</button>
+        
+    </body>
+
+</html>
